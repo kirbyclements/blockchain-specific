202330f1316e5686f999e192b1c845f310060e7f
Kirby Clements <kpc@Kirbys-MacBook-Pro.local>
2021-12-15 10:19:17 -0600

    first commit

---
+++ Shared/blockchain_specificApp.swift
@@ -0,0 +1,17 @@
+//
+//  blockchain_specificApp.swift
+//  Shared
+//
+//  Created by Kirby Clements on 11/7/21.
+//
+
+import SwiftUI
+
+@main
+struct blockchain_specificApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
